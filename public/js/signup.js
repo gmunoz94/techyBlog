e4f9b451a6d43d1eb55b336a67354ead46701ae2
gmunoz94 <g.munozii@icloud.com>
2021-08-13 18:08:42 -0500

    fixed login route

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,13 +2,14 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   //const username = document.querySelector('#username-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (email && password) {//(username && email && password)
     const response = await fetch("/api/user/register", {
       method: 'POST',
-      body: JSON.stringify({ email, password }), //username, email, password
+      body: JSON.stringify({ username, email, password }), //username, email, password
       headers: { 'Content-Type': 'application/json' },
     });
 
